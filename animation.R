9a1bb87c8e2dec3643bdbcd715cf8f7f41b69a9b
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-05-05 08:29:11 +0000

    that's a lot of plots

--- animation.R
+++ animation.R
@@ -2,9 +2,9 @@ library(magick)
 library(tictoc)
 
 tic()
-imgs <- image_read(list.files("plots/plot1", full.names = TRUE))
+imgs <- image_read(list.files("plots/plot6", full.names = TRUE))
 
 animation <- image_animate(image_scale(imgs, "300x300"), fps = 4, dispose = "previous")
 
-image_write(animation, "plot1.gif")
+image_write(animation, "plot6.gif")
 toc()
